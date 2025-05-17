be32ef658488fd94b017e4ef008de683c634677d
zokrezyl <zokrezyl@gmail.com>
2025-05-17 23:30:16 +0200

    collect artifacts

---
+++ bin/core-artifacts.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+
+set -e
+
+source bin/os.sh
+
+mkdir -p artifacts/lib artifacts/lib
+
+cp dawn/out/Release/src/dawn/native/libwebgpu_dawn.* artifacts/lib
+
+cp dawn/out/Release/Animometer artifacts/bin
+cp dawn/out/Release/ComputeBoids artifacts/bin
+cp dawn/out/Release/HelloTriangle artifacts/bin
+cp dawn/out/Release/DawnInfo artifacts/bin
+cp dawn/out/Release/ManualSurfaceTest artifacts/bin
+cp dawn/out/Release/tint artifacts/bin
+cp dawn/out/Release/tint_info artifacts/bin
+cp dawn/out/Release/tint_unittest artifacts/bin
+
+cp dawn/out/Release/src/dawn/native/libwebgpu_dawn.* artifacts/lib
+
