482b84f5dc9ae72b25955475397b2264e2c29109
zokrezyl <zokrezyl@gmail.com>
2025-05-17 22:00:16 +0200

    ni

--- bin/os.sh
+++ bin/os.sh
@@ -10,9 +10,18 @@ if [ $(uname -s) = "Linux" ]; then
 else
   if [ $(uname -s) = "Darwin" ]; then
     echo "MacOS detected"
+      bin/darwin-packages.sh
       export SDKROOT=$(xcrun --sdk macosx --show-sdk-path)
       export C_INCLUDE_PATH="$SDKROOT/usr/include"
-      export PATH="/usr/local/Cellar/llvm@19/19.1.7/bin":$PATH
+      #export PATH="/usr/local/Cellar/llvm@19/19.1.7/bin":$PATH
+
+      export LLVM_PATH="$(brew --prefix llvm)"
+      export PATH="$LLVM_PATH/bin:$PATH"
+      export LDFLAGS="-L$LLVM_PATH/lib"
+      export CPPFLAGS="-I$LLVM_PATH/include"
+      export CC=clang
+      export CXX=clang++
+
   else
     echo "Unknown OS detected"
   fi
