c7f0fd2a0a4fc71c8c73b48f1628652f99d4fab2
Zokre Zyl <zokrezyl@gmail.com>
2025-06-07 02:49:05 +0200

    ni

--- bin/core-artifacts.sh
+++ bin/core-artifacts.sh
@@ -13,19 +13,20 @@ if [[ -z "$OUT_NAME" ]]; then
   exit 1
 fi
 
-mkdir -p artifacts/${OUT_NAME}/lib artifacts/${OUT_NAME}/bin
-cp dawn/out/Release/src/dawn/native/libwebgpu_dawn.* artifacts/${OUT_NAME}/ib
-cp dawn/out/Release/Animometer artifacts/${OUT_NAME}/bin
-cp dawn/out/Release/ComputeBoids artifacts/${OUT_NAME}/bin
-cp dawn/out/Release/HelloTriangle artifacts/${OUT_NAME}/bin
-cp dawn/out/Release/DawnInfo artifacts/${OUT_NAME}/bin
-cp dawn/out/Release/ManualSurfaceTest artifacts/${OUT_NAME}/bin
-cp dawn/out/Release/tint artifacts/${OUT_NAME}/bin
-cp dawn/out/Release/tint_info artifacts/${OUT_NAME}/bin
-cp dawn/out/Release/tint_unittests artifacts/${OUT_NAME}/bin
+mkdir -p staging/${OUT_NAME}/lib staging/${OUT_NAME}/bin
+cp dawn/out/Release/src/dawn/native/libwebgpu_dawn.* staging/${OUT_NAME}/ib
+cp dawn/out/Release/Animometer staging/${OUT_NAME}/bin
+cp dawn/out/Release/ComputeBoids staging/${OUT_NAME}/bin
+cp dawn/out/Release/HelloTriangle staging/${OUT_NAME}/bin
+cp dawn/out/Release/DawnInfo staging/${OUT_NAME}/bin
+cp dawn/out/Release/ManualSurfaceTest staging/${OUT_NAME}/bin
+cp dawn/out/Release/tint staging/${OUT_NAME}/bin
+cp dawn/out/Release/tint_info staging/${OUT_NAME}/bin
+cp dawn/out/Release/tint_unittests staging/${OUT_NAME}/bin
 
 
+mkdir artifacts
 
-tar -czf "artifacts/${OUT_NAME}.tar.gz" artifacts/${OUT_NAME}
+tar -czf "artifacts/${OUT_NAME}.tar.gz" staging/${OUT_NAME}
 
-echo "✅ Created artifacts/${OUT_NAME}.tar.gz"
+echo "✅ Created staging/${OUT_NAME}.tar.gz"
