6625e038a6f75fd987e31ebee3a1bbbeb1341f85
Zokre Zyl <zokrezyl@gmail.com>
2025-06-07 02:29:19 +0200

    Update core-artifacts.sh

--- bin/core-artifacts.sh
+++ bin/core-artifacts.sh
@@ -5,18 +5,27 @@ set -e
 
 source bin/os.sh
 
-mkdir -p artifacts/lib artifacts/bin
 
-cp dawn/out/Release/src/dawn/native/libwebgpu_dawn.* artifacts/lib
 
-cp dawn/out/Release/Animometer artifacts/bin
-cp dawn/out/Release/ComputeBoids artifacts/bin
-cp dawn/out/Release/HelloTriangle artifacts/bin
-cp dawn/out/Release/DawnInfo artifacts/bin
-cp dawn/out/Release/ManualSurfaceTest artifacts/bin
-cp dawn/out/Release/tint artifacts/bin
-cp dawn/out/Release/tint_info artifacts/bin
-cp dawn/out/Release/tint_unittests artifacts/bin
+OUT_NAME="$1"
+if [[ -z "$OUT_NAME" ]]; then
+  echo "❌ Missing archive name argument. Usage: $0 <output-name>"
+  exit 1
+fi
 
-cp dawn/out/Release/src/dawn/native/libwebgpu_dawn.* artifacts/lib
+mkdir -p artifacts/${OUT_NAME}/lib artifacts/${OUT_NAME}/bin
+cp dawn/out/Release/src/dawn/native/libwebgpu_dawn.* artifacts/${OUT_NAME}/ib
+cp dawn/out/Release/Animometer artifacts/${OUT_NAME}/bin
+cp dawn/out/Release/ComputeBoids artifacts/${OUT_NAME}/bin
+cp dawn/out/Release/HelloTriangle artifacts/${OUT_NAME}/bin
+cp dawn/out/Release/DawnInfo artifacts/${OUT_NAME}/bin
+cp dawn/out/Release/ManualSurfaceTest artifacts/${OUT_NAME}/bin
+cp dawn/out/Release/tint artifacts/${OUT_NAME}/bin
+cp dawn/out/Release/tint_info artifacts/${OUT_NAME}/bin
+cp dawn/out/Release/tint_unittests artifacts/${OUT_NAME}/bin
 
+
+
+tar -czf "artifacts/${OUT_NAME}.tar.gz" artifacts/${OUT_NAME}
+
+echo "✅ Created artifacts/${OUT_NAME}.tar.gz"
